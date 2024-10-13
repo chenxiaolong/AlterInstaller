66757adf38f92394beef3bc36e662f570a1da830
Andrew Gunnerson <accounts+github@chiller3.com>
2024-10-13 18:41:08 -0400

    Add missing license headers
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Andrew Gunnerson
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
 plugins {
     alias(libs.plugins.android.application) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Andrew Gunnerson
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
