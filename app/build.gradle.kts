chenxiaolong/AlterInstaller

commit a5f149dfb3879bf1898e14b05084426c000a6bb4

Update dependencies and target API 36
Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
@@ -113,8 +113,8 @@ val archiveDir = extraDir.map { it.dir("archive") }
 android {
     namespace = "com.chiller3.alterinstaller"
 
-    compileSdk = 34
-    buildToolsVersion = "34.0.0"
+    compileSdk = 36
+    buildToolsVersion = "36.0.0"
 
     defaultConfig {
         applicationId = "com.chiller3.alterinstaller"
@@ -144,8 +144,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_17)
-        targetCompatibility(JavaVersion.VERSION_17)
+        sourceCompatibility(JavaVersion.VERSION_21)
+        targetCompatibility(JavaVersion.VERSION_21)
     }
 }
 

app/src/main/java/com/chiller3/alterinstaller/AlterInstallerSerializer.java
@@ -195,7 +195,7 @@ public class AlterInstallerSerializer implements XmlSerializer {
             throw new IllegalStateException("Tag stack is empty");
         }
 
-        String prev = tags.remove(tags.size() - 1);
+        String prev = tags.removeLast();
         if (!TextUtils.equals(prev, name)) {
             throw new IllegalStateException("Expected to pop " + name + ", but have " + prev);
         }

.github/workflows/ci.yml
@@ -1,4 +1,3 @@
----
 on:
   push:
     branches:
@@ -10,15 +9,15 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - name: Check out repository
-        uses: actions/checkout@v4
+        uses: actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683 # v4.2.2
         with:
           fetch-depth: 0
 
       - name: Validate gradle wrapper checksum
-        uses: gradle/actions/wrapper-validation@v3
+        uses: gradle/actions/wrapper-validation@ac638b010cf58a27ee6c972d7336334ccaf61c96 # v4.4.1
 
       - name: Set up JDK 21
-        uses: actions/setup-java@v4
+        uses: actions/setup-java@c5195efecf7bdfc987ee8bae7a71cb8b11521c00 # v4.7.1
         with:
           distribution: 'temurin'
           java-version: 21

.github/workflows/release.yml
@@ -1,4 +1,3 @@
----
 on:
   push:
     # Uncomment to test against a branch
@@ -24,10 +23,10 @@ jobs:
           echo "version=${version}" >> "${GITHUB_OUTPUT}"
 
       - name: Check out repository
-        uses: actions/checkout@v4
+        uses: actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683 # v4.2.2
 
       - name: Create release
-        uses: softprops/action-gh-release@v2
+        uses: softprops/action-gh-release@72f2c25fcb47643c292f7107632f7a47c1df5cd8 # v2.3.2
         with:
           tag_name: v${{ steps.get_version.outputs.version }}
           name: Version ${{ steps.get_version.outputs.version }}

gradle/libs.versions.toml
@@ -1,7 +1,7 @@
 [versions]
-android-gradle-plugin = "8.5.1"
-jgit = "6.10.0.202406032230-r"
-json = "20240303"
+android-gradle-plugin = "8.10.1"
+jgit = "7.3.0.202506031305-r"
+json = "20250517"
 
 [libraries]
 # Build script

gradle/wrapper/gradle-wrapper.properties
@@ -1,7 +1,7 @@
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
-distributionSha256Sum=d725d707bfabd4dfdc958c624003b3c80accc03f7037b5122c4b1d0ef15cecab
-distributionUrl=https\://services.gradle.org/distributions/gradle-8.9-bin.zip
+distributionSha256Sum=7197a12f450794931532469d4ff21a59ea2c1cd59a3ec3f89c035c3c420a6999
+distributionUrl=https\://services.gradle.org/distributions/gradle-8.14.2-bin.zip
 networkTimeout=10000
 validateDistributionUrl=true
 zipStoreBase=GRADLE_USER_HOME

commit 4c5a6a7b32a8c38f0bbbc34b0fc35742ea5ddd88

Fix compatibility with Android 12-14
Regression from: a5f149dfb3879bf1898e14b05084426c000a6bb4

Fixes: #15

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/build.gradle.kts
@@ -118,7 +118,7 @@ android {
 
     defaultConfig {
         applicationId = "com.chiller3.alterinstaller"
-        minSdk = compileSdk
+        minSdk = 31
         targetSdk = compileSdk
         versionCode = gitVersionCode
         versionName = gitVersionName
@@ -404,10 +404,10 @@ tasks.register("changelogUpdateLinks") {
 }
 
 tasks.register("changelogPreRelease") {
-    doLast {
-        val version = project.property("releaseVersion")
+    val version = project.findProperty("releaseVersion")
 
-        updateChangelog(version.toString(), true)
+    doLast {
+        updateChangelog(version!!.toString(), true)
         updateModuleChangelog("v$version")
     }
 }

app/src/main/java/com/chiller3/alterinstaller/AlterInstallerSerializer.java
@@ -195,7 +195,9 @@ public class AlterInstallerSerializer implements XmlSerializer {
             throw new IllegalStateException("Tag stack is empty");
         }
 
-        String prev = tags.removeLast();
+        // Android Studio suggests removeLast() despite our minSdk version not supporting it.
+        //noinspection SequencedCollectionMethodCanBeUsed
+        String prev = tags.remove(tags.size() - 1);
         if (!TextUtils.equals(prev, name)) {
             throw new IllegalStateException("Expected to pop " + name + ", but have " + prev);
         }

app/src/main/java/com/chiller3/alterinstaller/Main.java
@@ -122,7 +122,7 @@ public class Main {
     private static HashMap<String, PackageConfig> parsePropConfig(String path) throws IOException {
         Properties properties = new Properties();
 
-        try (FileReader reader = new FileReader(path, StandardCharsets.UTF_8)) {
+        try (FileReader reader = new FileReader(path)) {
             properties.load(reader);
         }
 